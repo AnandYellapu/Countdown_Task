e6254778042893de4a4958932d79358f81d0d84f
Yellapu AnandSai <104178105+AnandYellapu@users.noreply.github.com>
2022-10-26 16:51:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+var display=document.getElementById('content')
+setTimeout(()=>{
+    display.innerHTML='10'
+    setTimeout(()=>{
+        display.innerHTML='9'
+        setTimeout(()=>{
+            display.innerHTML='8'
+            setTimeout(()=>{
+                display.innerHTML='7'
+                setTimeout(()=>{
+                    display.innerHTML='6'
+                    setTimeout(()=>{
+                        display.innerHTML='5'
+                        setTimeout(()=>{
+                            display.innerHTML='4'
+                            setTimeout(()=>{
+                                display.innerHTML='3'
+                                setTimeout(()=>{
+                                    display.innerHTML='2'
+                                    setTimeout(()=>{
+                                        display.innerHTML='1'
+                                        setTimeout(()=>{
+                                            display.innerHTML='Happy Independence Day!!'
+                                            setTimeout(()=>{
+                                        
+                                            },1000)
+                                    
+                                        },1000)
+                                
+                                    },1000)
+                            
+                                },1000)
+                        
+                            },1000)
+                    
+                        },1000)
+                
+                    },1000)
+            
+                },1000)
+        
+            },1000)
+    
+        },1000)
+
+    },1000)
+})
+display()
